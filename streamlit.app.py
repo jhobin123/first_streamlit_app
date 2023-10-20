e5a5aa21aabb3af66fc65acf56e310b395594ff6
jhobin123 <147188064+jhobin123@users.noreply.github.com>
2023-10-20 10:54:29 -0400

    Update streamlit.app.py
    add new zection

--- streamlit.app.py
+++ streamlit.app.py
@@ -20,3 +20,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+#New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+#take the JSON version of the reponse and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Output it to the screen to normalize it
+streamlit.dataframe(fruityvice_normalized)
